235e4a55277b6000b4475ae48c11078eb8039437
Siqek <113335428+Siqek@users.noreply.github.com>
2024-04-25 18:25:57 +0200

    clock

---
+++ app.js
@@ -0,0 +1,69 @@
+let roughSvg = rough.svg(document.getElementById('svg'));
+
+var clock = document.getElementById("svgContainer");
+
+const __clockCenterX    = clock.offsetWidth / 2;
+const __clockCenterY    = clock.offsetHeight / 2;
+const __clockDiameter   = 300;
+
+const clockHands = Object.freeze([
+    {
+        name: 'second hand',
+        chLenght: 140,
+        seconds: 1,
+        ratio: 60,
+        line: null,
+        initLine: function () {this.line = roughSvg.line(__clockCenterX, __clockCenterY, __clockCenterX, __clockCenterY - this.chLenght)}
+    },
+    {
+        name: 'minute hand',
+        chLenght: 110,
+        seconds: 1 * 60,
+        ratio: 60,
+        line: null,
+        initLine: function () {this.line = roughSvg.line(__clockCenterX, __clockCenterY, __clockCenterX, __clockCenterY - this.chLenght)}
+    },
+    {
+        name: 'hour hand',
+        chLenght: 80,
+        seconds: 1 * 60 * 60,
+        ratio: 12,
+        line: null,
+        initLine: function () {this.line = roughSvg.line(__clockCenterX, __clockCenterY, __clockCenterX, __clockCenterY - this.chLenght)}
+    }
+]);
+
+(function initializeClockShape()
+{
+    drawCircle(__clockCenterX, __clockCenterY, __clockDiameter);
+    drawCircle(__clockCenterX, __clockCenterY, (__clockDiameter / 50));
+})();
+
+(function initializeClockHands()
+{
+    clockHands.forEach(clockHand => {
+            clockHand.initLine();
+            svg.appendChild(clockHand.line);
+    })
+})();
+
+(function initializeInterval()
+{
+    setInterval(() => {
+        const date = new Date();
+        const seconds = date.getSeconds() + date.getMinutes() * 60 + date.getHours() * 60 * 60;
+        clockHands.forEach(clockHand => {
+            clockHand.line.setAttribute('transform', `rotate(${((seconds / clockHand.seconds)%clockHand.ratio)/clockHand.ratio* 360}, ${__clockCenterX} ${__clockCenterY})`);
+        })
+    }, 1000)
+})();
+
+function drawCircle(x, y, diameter)
+{
+    const circle = roughSvg.circle(x, y, diameter, {
+        fill: 'rgba(0, 0, 0, 0.2)',
+        fillStyle: 'solid'
+    });
+
+    svg.appendChild(circle);
+}
